0d83a9981e18795aa598a07a6d7568a62328cad1
Shahrooz Ansari <shahrooz@uber.com>
2014-11-12 16:20:38 -0800

    beefing it up a bit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
-    res.end("Hello");
+    var body = "Uber Node.js course\n";
+    res.statusCode = 200;
+    res.setHeader('Content-length', body.length);
+    res.setHeader('Content-type', 'text/html');
+    res.end(body);
 });
 server.listen(9000, function() {
     console.log("listening on 9000");
