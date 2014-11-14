dd5b208e66082d85349693b615f61fa09b3b6931
Shahrooz Ansari <shahrooz@uber.com>
2014-11-13 18:16:52 -0800

    all REST methods added

--- shopping-list/server.js
+++ shopping-list/server.js
@@ -3,6 +3,20 @@ var url = require('url');
 var items = [];
 
 var server = http.createServer(function (req, res) {
+    function isValidIndex(index) {
+        if (isNaN(i)) {
+            res.statusCode = 400;
+            res.end('Item id not valid');
+            return false;
+        }
+        else if (!items[i]) {
+            res.statusCode = 404;
+            res.end('Item not found');
+            return false;
+        }
+        return true;
+    }
+
     switch (req.method) {
     case 'POST':
         var item = '';
@@ -12,8 +26,35 @@ var server = http.createServer(function (req, res) {
         });
         req.on('end', function () {
             items.push(item);
-            res.end('Item added\n');
+            res.end('Item added\n' + items);
         });
+        break;
+    case 'GET':
+        items.forEach(function (item, i) {
+            res.write(i + '. ' + item + '\n');
+        });
+        res.end();
+        break;
+    case 'DELETE':
+    case 'PUT':
+        var pathname = url.parse(req.url).pathname;
+        var i = parseInt(pathname.slice(1), 10);
+        if (isValidIndex(i)) {
+            if (req.method == 'DELETE') {
+                items.splice(i, 1);
+                res.end('Item deleted successfully');
+            } else {
+                //PUT
+                var newValue = url.parse(req.url).query;
+                if (newValue != null) {
+                    items[i] = newValue;
+                    res.end("Item " + i + " updated to " + newValue);    
+                } else {
+                    res.end("Failed to update Item " + i);
+                }
+                
+            }
+        }
         break;
     }
 });
